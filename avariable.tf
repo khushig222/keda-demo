8211f899453814b55692dd91f78c1d65c2a32456
khushig222 <khushi.g@intimetec.com>
2026-02-20 10:57:20 +0530

    Create avariable.tf

---
+++ avariable.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "East US 2"
+}
+
+variable "resource_group_name" {
+  default = "rg-keda-asb"
+}
+
+variable "namespace_name" {
+  default = "keda-asb-namespace"
+}
+
+variable "queue_name" {
+  default = "keda-asb"
+}
